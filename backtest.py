0b0d066a546f36acf38ce35aac578530c8a37434
Welly Tambunan <if05041@gmail.com>
2018-08-22 17:10:22 +0700

    remove all unused strategy

--- backtest.py
+++ backtest.py
@@ -8,22 +8,18 @@ def get_saham_list():
     fname = "sahamprofit.txt"
     with open(fname) as f:
         content = f.readlines()
-    # you may also want to remove whitespace characters like `\n` at the end of each line
     return [x.strip() for x in content]
 
 def run_backtest():
     symbols = ["BBCA", "BBRI", "UNTR", "BMRI", "TLKM"]
-    # symbols = ['BBCA']
+
     metrics = []
 
     for i, symbol in enumerate(symbols):
         try:
             strategy = StrategyFactory.factory("RSI", symbol)
             prices = pd.read_csv("stock/" + symbol + '.JK.csv', index_col=0).convert_objects(convert_numeric=True)
-            # prices = prices[prices.index > "2013-01-01"]
-            # prices = prices[prices.index > "2018-01-01"]
             prices = prices[prices.index > "2007-01-01"]
-            # prices = prices[prices.index > "2017-01-01"]
             for i in range(1, len(prices)):
                 current_price = prices.iloc[[i]]
                 current_candle = Candle(current_price.index[0], current_price.High[0], current_price.Low[0],
@@ -31,7 +27,6 @@ def run_backtest():
 
                 d = {symbol: current_candle}
                 strategy.on_tick(d)
-                # print("")
             metrics.append(strategy.calculate())
         except Exception as err:
             print("error : " + symbol + " " + format(err))
--- strategy_factory.py
+++ strategy_factory.py
@@ -1,17 +1,6 @@
 # Create based on class name:
-from adx_strategy import ADXStrategy
-from atr_strategy import ATRStrategy
-from bollinger_strategy import BollingerStrategy
-from double7_strategy import Double7Strategy
-from macd_strategy import MACDStrategy
 from moving_average import MovingAverageStrategy
-from purnaye_strategy import PurnayeStrategy
-from rsi_cum_strategy import RSICumStrategy
-from rsi_holy_strategy import RSIHolyStrategy
-from rsi_powerzone_strategy import RSIPowerZoneStrategy
 from rsi_strategy import RSIStrategy
-from stoch_strategy import StochStrategy
-from wvf_strategy import WVFStrategy
 
 
 class StrategyFactory:
